8c983ea9fdd3896d2d5c196301090eaea74864d0
hannut91 <hannut91@gmail.com>
2019-06-29 00:39:05 +0900

    Update balance response type

--- types/balance.go
+++ types/balance.go
@@ -1,9 +1,9 @@
 package types
 
 type Balance struct {
-	Currency       string `json:"currency"`
-	Balance        string `json:"balance"`
-	Locked         string `json:"locked"`
-	AvgKrwBuyPrice string `json:"avg_krw_buy_price"`
-	Modified       bool   `json:"modified"`
+	Currency            string `json:"currency"`
+	Balance             string `json:"balance"`
+	Locked              string `json:"locked"`
+	AvgBuyPrice         string `json:"avg_buy_price"`
+	AvgBuyPriceModified bool   `json:"avg_buy_price_modified"`
 }
